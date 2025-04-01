efb793864151938c51dc4f10ce3c16d3c04720dc
safalafas <safal@ismt.edu.np>
2025-03-31 20:24:47 -0400

    Fix resource names in Terraform configuration and add output for access key

--- infra/tf-backend/main.tf
+++ infra/tf-backend/main.tf
@@ -12,12 +12,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "tf_backend_rg" {
-  name     = "chau0317-githubactions-rg"
+  name     = "chau0317-githubaction-rg"
   location = "canadacentral"
 }
 
 resource "azurerm_storage_account" "tf_backend_sa" {
-  name                     = "chau0317githubactions"
+  name                     = "chau0317githubaction"
   resource_group_name      = azurerm_resource_group.tf_backend_rg.name
   location                 = azurerm_resource_group.tf_backend_rg.location
   account_tier             = "Standard"
@@ -29,4 +29,9 @@ resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.tf_backend_sa.name
   container_access_type = "private"
+}
+
+output "arm_access_key" {
+  value     = azurerm_storage_account.tf_backend_sa.primary_access_key
+  sensitive = true
 }
