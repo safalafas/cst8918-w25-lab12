4d94f5c9b08eaecf4e8ed19a397bdcabff69540c
safalafas <safal@ismt.edu.np>
2025-03-31 23:57:25 -0400

    Add Azure virtual network and subnet resources to Terraform configuration

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "app_rg" {
   name     = "chau0317-a12-rg"
   location = "canadacentral"
 }
+
+resource "azurerm_virtual_network" "lab_vnet" {
+  name                = "lab-vnet"
+  location            = azurerm_resource_group.tf_app_rg.location
+  resource_group_name = azurerm_resource_group.tf_app_rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "lab_subnet" {
+  name                 = "lab-subnet"
+  resource_group_name  = azurerm_resource_group.tf_app_rg.name
+  virtual_network_name = azurerm_virtual_network.lab_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
